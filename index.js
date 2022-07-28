a19f91e8c8f4849deb82b6d88a0f9494794acc9b
Noah Solis <NSolis1993@gmail.com>
2022-07-28 16:02:33 -0500

    commit tree 95b2420ebfa19bfdd4d91dd3508f13cd5445e7b9

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Your code here
+const createEmployeeRecord = (field) => {
+    return {
+        firstName: field[0],
+        familyName: field[1],
+        title: field[2],
+        payPerHour: field[3],
+        timeInEvents: [],
+        timeOutEvents: [],
+    }
+}
+
+const createEmployeeRecords = (employeeInfo) => {
+    return employeeInfo.map((field) => {
+        return createEmployeeRecord(field)
+    })
+}
+
+const createTimeInEvent = (employee, timeStamp) => {
+    
+}
